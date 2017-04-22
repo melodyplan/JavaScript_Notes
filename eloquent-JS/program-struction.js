18e144cb684cc1a0f2d2986c2f23969030e65878
Melody Plan <melodyplan@gmail.com>
2017-04-22 16:02:51 -0700

    eloquent js - program structure - exercise 1

---
+++ eloquent-JS/program-struction.js
@@ -0,0 +1,33 @@
+for (var triangle = "#######"; triangle <= triangle.length; triangle += 1)
+//while (triangle <= triangle.length) {
+  console.log(triangle.length);
+  //triangle = triangle + 1;
+//}
+
+//for (var number = 0; number <= 12; number += 2)
+  //console.log(number);
+
+var number = 0;
+while (number <= 7) {
+  console.log(number);
+  number = number + 1;
+}
+
+var triangle = "#";
+while (triangle <= "#######") {
+  console.log(triangle);
+  triangle = triangle += "#";
+}
+
+/*it works!!!! it only took me like 20 minutes to figure out, but g'damn it,
+it works!! it logs:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+just like it's supposed to yaaaay*/
