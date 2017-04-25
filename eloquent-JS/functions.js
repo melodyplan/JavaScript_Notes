0a5e6e1fe9aa99c34e8a5935ebab14f8079b05f5
Melody Plan <melodyplan@gmail.com>
2017-04-24 18:18:13 -0700

    functions exercises, find the minimum

---
+++ eloquent-JS/functions.js
@@ -0,0 +1,13 @@
+var min = function(x, y) {
+	if (x > y)
+      return y;
+    else (y > x)
+      return x;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+//I tried this and I was shocked it worked the first time and I got it right away! =D
